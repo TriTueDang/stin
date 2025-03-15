f9f656f64f1402f43103c84372d38d25ed101d08
Tue Dang Tri <tri.tue.dang@tul.cz>
2025-03-15 08:40:11 +0100

    spring-boot

---
+++ src/main/java/tul/semestralkaSTIN/SemestralkaStinApplication.java
@@ -0,0 +1,13 @@
+package tul.semestralkaSTIN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestralkaStinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestralkaStinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tul/semestralkaSTIN/SemestralkaStinApplicationTests.java
@@ -0,0 +1,13 @@
+package tul.semestralkaSTIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemestralkaStinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
